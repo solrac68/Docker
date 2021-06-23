701cea2d748ec14b3cf6981bfe6293403a98ed69
Carlos <solrac.hincapie@gmail.com>
2021-06-23 20:55:09 +0000

    volumnes

---
+++ CommandLinux.sh
@@ -0,0 +1,15 @@
+$ chmod +x resize.sh
+$ sh resize.sh 30
+% Total    % Received % Xferd  Average Speed   Time    Time     Time  Current
+                                 Dload  Upload   Total   Spent    Left  Speed
+100    19  100    19    0     0   1187      0 --:--:-- --:--:-- --:--:--  1187
+.
+.
+$ df -h
+Filesystem      Size  Used Avail Use% Mounted on
+udev            476M     0  476M   0% /dev
+tmpfs            98M  800K   98M   1% /run
+/dev/xvda1       30G  6.0G   24G  21% /
+tmpfs           490M     0  490M   0% /dev/shm
+tmpfs           5.0M     0  5.0M   0% /run/lock
+tmpfs           490M     0  490M   0% /sys/fs/cgroup
