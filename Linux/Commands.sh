01d099b5ba1986eb1b8401047d5bbaf680fa196e
Carlos <solrac.hincapie@gmail.com>
2021-06-26 14:21:24 +0000

    56. Conceptos de volúmenes

---
+++ Linux/Commands.sh
@@ -0,0 +1,7 @@
+# Información del comando pwd
+$ man pwd 
+$ ls /home/ubuntu -l
+total 12
+drwxr-xr-x 6 ubuntu ubuntu 4096 Jun 25 15:51 environment
+drwxr-xr-x 5 ubuntu ubuntu 4096 Jun 21 11:13 node_modules
+-rw-r--r-- 1 ubuntu ubuntu  746 Jun 21 11:13 package-lock.json
