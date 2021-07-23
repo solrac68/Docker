70ac0c1bc47a8176db339b536857da9c19e60e8d
solrac68 <solrac.hincapie@gmail.com>
2021-07-23 17:51:59 -0500

    Linux df

--- Linux/Commands.sh
+++ Linux/Commands.sh
@@ -81,3 +81,123 @@ carlos:~/environment/Docker (main) $
 # Directorio padre de padre
 carlos:~/environment/Docker (main) $ cd ../..
 carlos:~ $ 
+
+# Historia
+➜  Proyectos history
+.
+.
+2354  ls -lt
+2355  clear
+2356  ls
+2357  cd ..
+2358  ls -l
+2359  ls -l go
+2360  history
+2361  ls
+
+➜  Proyectos !2354
+➜  Proyectos ls -lt
+
+#15. Crear, mover y borrar ficheros y directorios
+➜  Proyectos mkdir ejemplo_directorio
+➜  Proyectos cd ejemplo_directorio
+➜  ejemplo_directorio mkdir dir1 dir2 dir3
+➜  ejemplo_directorio ls -lt
+total 12
+drwxrwxr-x 2 carlos carlos 4096 jul 23 16:53 dir1
+drwxrwxr-x 2 carlos carlos 4096 jul 23 16:53 dir2
+drwxrwxr-x 2 carlos carlos 4096 jul 23 16:53 dir3
+
+➜  ejemplo_directorio mkdir dir1/fotos 
+➜  ejemplo_directorio mkdir dir1/videos
+➜  ejemplo_directorio ls -lt dir1 
+total 8
+drwxrwxr-x 2 carlos carlos 4096 jul 23 16:59 videos
+drwxrwxr-x 2 carlos carlos 4096 jul 23 16:59 fotos
+
+# Crea un directorio dentro de uno que no existe todavía (dir5 es creado)
+➜  ejemplo_directorio mkdir -p dir5/fotos
+➜  ejemplo_directorio ls -lt
+total 16
+drwxrwxr-x 3 carlos carlos 4096 jul 23 17:02 dir5
+drwxrwxr-x 4 carlos carlos 4096 jul 23 16:59 dir1
+drwxrwxr-x 2 carlos carlos 4096 jul 23 16:53 dir2
+drwxrwxr-x 2 carlos carlos 4096 jul 23 16:53 dir3
+➜  ejemplo_directorio ls -lt dir5
+total 4
+drwxrwxr-x 2 carlos carlos 4096 jul 23 17:02 fotos
+
+# Borrar directorios y todo su contenido
+
+➜  ejemplo_directorio rm -r dir1
+➜  ejemplo_directorio ls -lt
+total 12
+drwxrwxr-x 3 carlos carlos 4096 jul 23 17:02 dir5
+drwxrwxr-x 2 carlos carlos 4096 jul 23 16:53 dir2
+drwxrwxr-x 2 carlos carlos 4096 jul 23 16:53 dir3
+
+➜  Linux git:(main) ✗ touch informe.txt
+➜  Linux git:(main) ✗ rm informe.txt 
+➜  Linux git:(main) ✗ ls-lt
+zsh: command not found: ls-lt
+➜  Linux git:(main) ✗ ls -lt
+total 8
+-rw-rw-r-- 1 carlos carlos 4446 jul 23 17:15 Commands.sh
+➜  Linux git:(main) ✗ 
+
+# Mover un archivo de una carpeta a otra.
+➜  dir1 git:(main) ✗ touch carta-1.txt
+➜  dir1 git:(main) ✗ mv ./carta-1.txt ../dir2/
+➜  dir1 git:(main) ✗ ls -lt ../dir2/
+total 4
+-rw-rw-r-- 1 carlos carlos 2414 jul 23 17:30 carta-1.txt
+
+# Renombrar un archivo
+➜  dir2 git:(main) ✗ ls -lt
+total 4
+-rw-rw-r-- 1 carlos carlos 2414 jul 23 17:30 carta-1.txt
+➜  dir2 git:(main) ✗ mv carta-1.txt carta.txt
+➜  dir2 git:(main) ✗ ls -lt
+total 4
+-rw-rw-r-- 1 carlos carlos 2414 jul 23 17:30 carta.txt
+
+# Mover un directorio
+➜  dir2 git:(main) ✗ mkdir dir21
+➜  dir2 git:(main) ✗ ls
+carta.txt  dir21
+➜  dir2 git:(main) ✗ mv dir21/ ../dir1
+➜  dir2 git:(main) ✗ ls -lt
+total 4
+-rw-rw-r-- 1 carlos carlos 2414 jul 23 17:30 carta.txt
+➜  dir2 git:(main) ✗ ls -lt ../dir1
+total 4
+drwxrwxr-x 2 carlos carlos 4096 jul 23 17:36 dir21
+
+# Comandos para conocer el espacio disponible
+
+carlos-VivoBook-15-ASUS-Laptop-X542UF# df
+S.ficheros     bloques de 1K   Usados Disponibles Uso% Montado en
+udev                 8109388        0     8109388   0% /dev
+tmpfs                1627752     2416     1625336   1% /run
+/dev/sda2          459924552 78907236   357584664  19% /
+tmpfs                8138760   109880     8028880   2% /dev/shm
+tmpfs                   5120        4        5116   1% /run/lock
+tmpfs                8138760        0     8138760   0% /sys/fs/cgroup
+/dev/loop1            241152   241152           0 100% /snap/audacity/910
+/dev/loop0            192384   192384           0 100% /snap/audacity/857
+/dev/loop3             56832    56832           0 100% /snap/core18/2066
+.
+.
+
+# Espacio usado desde la raiz del sistema
+carlos-VivoBook-15-ASUS-Laptop-X542UF# df -h /
+S.ficheros     Tamaño Usados  Disp Uso% Montado en
+/dev/sda2        439G    76G  342G  19% /
+
+# LO que ocupa el directorio actual
+carlos-VivoBook-15-ASUS-Laptop-X542UF# du -sh
+8,0K 
+
+carlos-VivoBook-15-ASUS-Laptop-X542UF# cd /home
+carlos-VivoBook-15-ASUS-Laptop-X542UF# pwd
+/home
